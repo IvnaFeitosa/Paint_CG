544e3c0fa0ac0901e489d9049a405d926636bf13
José Victor <victorlimaf55@gmail.com>
2025-09-11 17:55:46 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(800, 600);
     glutInitWindowPosition(250, 50);
     glutCreateWindow("Paint_CG");
